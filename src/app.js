fff24f21e12b56f81a5b6d56134544f4ce12b353
anshy03 <1ms23is017@msrit.edu>
2026-03-15 15:43:47 +0530

    add root route

--- src/app.js
+++ src/app.js
@@ -14,6 +14,9 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+app.get("/", (req, res) => {
+  res.send("vidTube backend is running 🚀");
+});
 // Routes
 import healthcheckRouter from "./routes/healthcheck.routes.js"
 import userRouter from "./routes/user.routes.js"
